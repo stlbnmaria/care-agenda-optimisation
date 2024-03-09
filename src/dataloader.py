4c96d7a0797ac47d1c34d3921ccf8a2a788f7188
Maria Stoelben <maria.stlbn@hotmail.de>
2024-03-09 23:10:17 +0100

    init creating scheduling df in dataloader

--- src/dataloader.py
+++ src/dataloader.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from pathlib import Path
 from typing import Tuple
 
@@ -82,6 +83,78 @@ def create_commute_df(kind: str = "driving") -> None:
     commute_data_df.to_csv(f"data/commute_{kind}_all.csv", index=False)
 
 
+def create_schedule_df() -> None:
+    excel_file = Path("data/ChallengeXHEC23022024.xlsx")
+    schedule = pd.read_excel(excel_file, sheet_name=0)
+    caregivers = pd.read_excel(excel_file, sheet_name=2)
+
+    sched_one_day = schedule.loc[schedule.Date == "2024-01-03", :]
+
+    discard_list = [
+        "ADMINISTRATION",
+        "VISITE MEDICALE",
+        "FORMATION",
+        "COORDINATION",
+        "HOMMES TOUTES MAINS",
+    ]
+
+    sched_one_day = sched_one_day[~sched_one_day.Prestation.isin(discard_list)]
+
+    sched_one_day["idx"] = sched_one_day.index
+
+    common_date = sched_one_day["Date"].iloc[0]
+    sched_one_day["Heure de fin"] = sched_one_day["Heure de fin"].apply(
+        lambda x: datetime.combine(common_date, x)
+    )
+    sched_one_day["Heure de début"] = sched_one_day["Heure de début"].apply(
+        lambda x: datetime.combine(common_date, x)
+    )
+    sched_one_day["Duration"] = (
+        sched_one_day["Heure de fin"] - sched_one_day["Heure de début"]
+    )
+    sched_one_day["Duration"] = sched_one_day["Duration"].apply(
+        lambda x: x.seconds // 60
+    )
+
+    before_first_client = caregivers[["ID Intervenant"]].copy()
+    before_first_client["ID Client"] = caregivers["ID Intervenant"]
+    before_first_client["Duration"] = 0
+    before_first_client["Date"] = pd.to_datetime("2024-01-03")
+    before_first_client["Heure de début"] = before_first_client[
+        "Date"
+    ] + pd.Timedelta(hours=5)
+    before_first_client["Heure de fin"] = before_first_client[
+        "Date"
+    ] + pd.Timedelta(hours=5)
+    before_first_client["Prestation"] = "COMMUTE"
+    before_first_client["idx"] = before_first_client.index
+
+    after_last_client = caregivers[["ID Intervenant"]].copy()
+    after_last_client["ID Client"] = caregivers["ID Intervenant"]
+    after_last_client["Duration"] = 0
+    after_last_client["Date"] = pd.to_datetime("2024-01-03")
+    after_last_client["Heure de début"] = after_last_client[
+        "Date"
+    ] + pd.Timedelta(hours=22)
+    after_last_client["Heure de fin"] = after_last_client[
+        "Date"
+    ] + pd.Timedelta(hours=22)
+    after_last_client["Prestation"] = "COMMUTE"
+    after_last_client["idx"] = before_first_client.index + 10000
+
+    sched_one_day = pd.concat(
+        [sched_one_day, before_first_client], ignore_index=True
+    )
+    sched_one_day = pd.concat(
+        [sched_one_day, after_last_client], ignore_index=True
+    )
+
+    sched_one_day = sched_one_day.sort_values("Heure de début")
+    sched_one_day.to_csv("data/schedule_2024-03-01.csv", index=False)
+
+
 if __name__ == "__main__":
     create_commute_df(kind="driving")
     # create_commute_df(kind="license")
+
+    create_schedule_df()
