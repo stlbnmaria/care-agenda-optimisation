67b809e23c1ab5b8b11c852ef33090c3784638af
Maria Stoelben <maria.stlbn@hotmail.de>
2024-03-13 20:25:14 +0100

    remove absolute filepath

--- app/pages/1_📊_Data_Analysis.py
+++ app/pages/1_📊_Data_Analysis.py
@@ -1,21 +1,22 @@
 """The module to display data analysis in the app."""
-import os
 import io
+import os
+from zipfile import ZipFile
+
+import folium
+import holidays
+import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import seaborn as sns
-import plotly.io as pio
-import plotly.figure_factory as ff
-import folium
-from streamlit_folium import folium_static
-import streamlit as st
-from zipfile import ZipFile
-import matplotlib.pyplot as plt
-from streamlit_extras.switch_page_button import switch_page
 import plotly.express as px
+import plotly.figure_factory as ff
 import plotly.graph_objects as go
-import holidays
+import plotly.io as pio
+import seaborn as sns
+import streamlit as st
 from plotly.subplots import make_subplots
+from streamlit_extras.switch_page_button import switch_page
+from streamlit_folium import folium_static
 
 st.set_page_config(page_title="Data Analysis", page_icon="📊")
 
@@ -38,18 +39,17 @@ def read_excel_file(excel_file: str) -> tuple:
     return tuple(dataframes)
 
 
-
 @st.cache_data()
 def preloaded():
-    file_path = "/Users/madhuranirale/Desktop/HEC-T1/health/ChallengeXHEC23022024.xlsx"
+    file_path = "../data/ChallengeXHEC23022024.xlsx"
     excel_data = pd.ExcelFile(file_path)
-
 
     jan24_df = pd.read_excel(excel_data, sheet_name="JAN24")
     clients_df = pd.read_excel(excel_data, sheet_name="clients")
     intervenants_df = pd.read_excel(excel_data, sheet_name="intervenants")
 
     return jan24_df, clients_df, intervenants_df
+
 
 def display_kpis(kpi_names, kpi_values):
     """
@@ -120,38 +120,33 @@ if (
     # Clients filter
     client_options = ["All"] + list(jan24_df["ID Client"].unique())
     selected_clients = st.sidebar.multiselect(
-        "Clients", client_options, default= "All"
+        "Clients", client_options, default="All"
     )
     if "All" in selected_clients:
         selected_clients = list(jan24_df["ID Client"].unique())
 
     # Intervenant filter
     caregiver_options = ["All"] + list(jan24_df["ID Intervenant"].unique())
-    selected_caregivers = st.sidebar.multiselect("Caregiver", caregiver_options, default="All")
+    selected_caregivers = st.sidebar.multiselect(
+        "Caregiver", caregiver_options, default="All"
+    )
     if "All" in selected_caregivers:
         selected_caregivers = list(jan24_df["ID Intervenant"].unique())
 
     # Services filter
     service_options = ["All"] + list(jan24_df["Prestation"].unique())
-    selected_services = st.sidebar.multiselect("Service", service_options, default="All")
+    selected_services = st.sidebar.multiselect(
+        "Service", service_options, default="All"
+    )
     if "All" in selected_services:
         selected_services = list(jan24_df["Prestation"].unique())
 
     # Apply filters
     filtered_df = jan24_df[
-        (
-            (jan24_df["ID Client"].isin(selected_clients))
-        )
-        & (
-            (jan24_df["ID Intervenant"].isin(selected_caregivers))
-        )
-        & (
-            (jan24_df["Prestation"].isin(selected_services))
-        )
-        & (
-            (jan24_df["Date"] >= start_date)
-            & (jan24_df["Date"] <= end_date)
-        )
+        ((jan24_df["ID Client"].isin(selected_clients)))
+        & ((jan24_df["ID Intervenant"].isin(selected_caregivers)))
+        & ((jan24_df["Prestation"].isin(selected_services)))
+        & ((jan24_df["Date"] >= start_date) & (jan24_df["Date"] <= end_date))
     ]
 
     st.subheader("Key Metrics")
@@ -159,26 +154,39 @@ if (
     # Compute key metrics
     total_clients = len(clients_df)
     total_caregivers = len(intervenants_df)
-    vehicule_personnel_percentage = (intervenants_df['Véhicule personnel'].value_counts(normalize=True) * 100).get('Oui', 0)
-    vehicule_personnel_percentage_formatted = f"{vehicule_personnel_percentage:.2f}%"
-    permis_percentage = (intervenants_df['Permis'].value_counts(normalize=True) * 100).get('Oui', 0)
+    vehicule_personnel_percentage = (
+        intervenants_df["Véhicule personnel"].value_counts(normalize=True)
+        * 100
+    ).get("Oui", 0)
+    vehicule_personnel_percentage_formatted = (
+        f"{vehicule_personnel_percentage:.2f}%"
+    )
+    permis_percentage = (
+        intervenants_df["Permis"].value_counts(normalize=True) * 100
+    ).get("Oui", 0)
     permis_percentage_formatted = f"{permis_percentage:.2f}%"
-    competences_list = [comp.split(', ') for comp in intervenants_df['Compétences']]
-    flatten_competences = [item for sublist in competences_list for item in sublist]
-    most_common_competence = str.lower(pd.Series(flatten_competences).value_counts().idxmax())
+    competences_list = [
+        comp.split(", ") for comp in intervenants_df["Compétences"]
+    ]
+    flatten_competences = [
+        item for sublist in competences_list for item in sublist
+    ]
+    most_common_competence = str.lower(
+        pd.Series(flatten_competences).value_counts().idxmax()
+    )
 
     # Example KPIs with emojis
     kpi_names = [
         ":office_worker: #Clients",
         ":busts_in_silhouette: #Caregivers",
         ":red_car: License holders"
-        #":mechanic: Top service",
+        # ":mechanic: Top service",
     ]
     kpi_values = [
         total_clients,
         total_caregivers,
         permis_percentage_formatted
-        #most_common_competence
+        # most_common_competence
     ]
 
     display_kpis(kpi_names, kpi_values)
@@ -186,7 +194,7 @@ if (
     st.write("\n")
     st.write("\n")
 
-    st.subheader('Client and Caregiver locations')
+    st.subheader("Client and Caregiver locations")
 
     # Create a Folium map
     map = folium.Map(
@@ -228,50 +236,98 @@ if (
     st.subheader("Service distribution")
 
     # Group by 'Prestation' and count unique 'ID Client' for each service
-    clients_per_service = filtered_df.groupby('Prestation')['ID Client'].nunique()
+    clients_per_service = filtered_df.groupby("Prestation")[
+        "ID Client"
+    ].nunique()
 
     # Plotting the pie chart
     plt.figure(figsize=(10, 10))
     ax = plt.subplot()
-    wedges, _ = ax.pie(clients_per_service, labels=clients_per_service.index, startangle=140, wedgeprops=dict(width=0.4))
+    wedges, _ = ax.pie(
+        clients_per_service,
+        labels=clients_per_service.index,
+        startangle=140,
+        wedgeprops=dict(width=0.4),
+    )
 
     # Draw a white circle at the center to create the donut shape
-    center_circle = plt.Circle((0, 0), 0.6, color='white', fc='white', linewidth=1.25)
+    center_circle = plt.Circle(
+        (0, 0), 0.6, color="white", fc="white", linewidth=1.25
+    )
     ax.add_artist(center_circle)
 
     # Display legend with percentages in brackets, reduce the size
-    legend_labels = [f'{label} ({percent:.1f}%)' for label, percent in zip(clients_per_service.index, clients_per_service / clients_per_service.sum() * 100)]
-    ax.legend(wedges, legend_labels, title="Services", loc="lower left", bbox_to_anchor=(1, 0.7))
-    
+    legend_labels = [
+        f"{label} ({percent:.1f}%)"
+        for label, percent in zip(
+            clients_per_service.index,
+            clients_per_service / clients_per_service.sum() * 100,
+        )
+    ]
+    ax.legend(
+        wedges,
+        legend_labels,
+        title="Services",
+        loc="lower left",
+        bbox_to_anchor=(1, 0.7),
+    )
+
     st.pyplot(plt)
 
     st.write("\n")
     st.write("\n")
 
-    st.subheader('Total Number of Services Provided Per Day')
+    st.subheader("Total Number of Services Provided Per Day")
 
-    services_per_day = filtered_df.groupby('Date').size()
+    services_per_day = filtered_df.groupby("Date").size()
 
     # Plotting the data
     plt.figure(figsize=(12, 6))
-    services_per_day.plot(marker='o', color='blue')
-    plt.xlabel('Date')
-    plt.ylabel('Number of Services')
+    services_per_day.plot(marker="o", color="blue")
+    plt.xlabel("Date")
+    plt.ylabel("Number of Services")
     plt.xticks(rotation=45)
     plt.grid(False)
 
     st.pyplot(plt)
 
-    st.subheader('Service Frequency by Day of the Week')
+    st.subheader("Service Frequency by Day of the Week")
 
-    filtered_df['Day of Week'] = filtered_df['Date'].dt.day_name()
-    days_order = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-    filtered_df['Day of Week'] = pd.Categorical(filtered_df['Day of Week'], categories=days_order, ordered=True)
-    service_by_day = filtered_df.groupby(['Prestation', 'Day of Week']).size().unstack(fill_value=0)
+    filtered_df["Day of Week"] = filtered_df["Date"].dt.day_name()
+    days_order = [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday",
+    ]
+    filtered_df["Day of Week"] = pd.Categorical(
+        filtered_df["Day of Week"], categories=days_order, ordered=True
+    )
+    service_by_day = (
+        filtered_df.groupby(["Prestation", "Day of Week"])
+        .size()
+        .unstack(fill_value=0)
+    )
 
-    fig = px.bar(service_by_day, barmode='group', labels={'value': 'Frequency', 'variable': 'Day of Week'})
+    fig = px.bar(
+        service_by_day,
+        barmode="group",
+        labels={"value": "Frequency", "variable": "Day of Week"},
+    )
     fig.update_layout(width=850, height=600)
-    fig.update_layout(legend=dict(title='Day of Week', orientation='h', yanchor='bottom', y=1.02, xanchor='right', x=1))
+    fig.update_layout(
+        legend=dict(
+            title="Day of Week",
+            orientation="h",
+            yanchor="bottom",
+            y=1.02,
+            xanchor="right",
+            x=1,
+        )
+    )
 
     st.plotly_chart(fig)
 
